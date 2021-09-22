9456154bf55393b05e863f94291e098baf86d6f3
Nils Christian Ehmke <nils@rhocas.de>
2021-09-22 20:51:57 +0200

    [#61] Consider making AsciiDocElement a functional interface
    Signed-off-by: Nils Christian Ehmke <nils@rhocas.de>

--- integration/asciidoc/src/main/java/org/archifacts/integration/asciidoc/AsciiDocElement.java
+++ integration/asciidoc/src/main/java/org/archifacts/integration/asciidoc/AsciiDocElement.java
@@ -1,6 +1,17 @@
 package org.archifacts.integration.asciidoc;
 
+/**
+ * Implementations of this interface are considered renderable AsciiDoc elements. The implementations  
+ * are responsible themselves for providing valid AsciiDoc.
+ */
+@FunctionalInterface
 public interface AsciiDocElement {
 
+	/**
+	 * Renders the current element as a String.
+	 * 
+	 * @return The AsciiDoc String representing the element. Should not be <code>null</code>.
+	 */
 	String render();
+	
 }
