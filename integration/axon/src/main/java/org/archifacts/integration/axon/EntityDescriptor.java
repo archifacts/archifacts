a8a150e2d80c6acf4bf9314595ca491e47c37232
Oliver Libutzki <oliver@libutzki.de>
2021-10-19 19:57:29 +0200

    Axon EntityDescriptor and SagaDescriptor added

---
+++ integration/axon/src/main/java/org/archifacts/integration/axon/EntityDescriptor.java
@@ -0,0 +1,69 @@
+package org.archifacts.integration.axon;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.archifacts.core.descriptor.BuildingBlockDescriptor;
+import org.archifacts.core.model.BuildingBlockType;
+import org.axonframework.modelling.command.AggregateMember;
+
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaField;
+import com.tngtech.archunit.core.domain.JavaMember;
+import com.tngtech.archunit.core.domain.JavaMethod;
+import com.tngtech.archunit.core.domain.JavaParameterizedType;
+import com.tngtech.archunit.core.domain.JavaType;
+
+public class EntityDescriptor implements BuildingBlockDescriptor{
+
+	private static final BuildingBlockType TYPE = BuildingBlockType.of("Entity");
+
+	@Override
+	public BuildingBlockType type() {
+		return TYPE;
+	}
+
+	@Override
+	public boolean isBuildingBlock(JavaClass javaClass) {
+		return javaClass.getDirectDependenciesToSelf()
+			.stream()
+			.flatMap(dep -> dep.getOriginClass().getMembers().stream())
+			.filter(field -> field.isMetaAnnotatedWith(AggregateMember.class))
+			.map(this::getType)
+			.filter(this::isNotUntypedCollectionOrMap)
+			.map(this::getJavaClass)
+			.anyMatch(javaClass::equals);
+	}
+	
+	private JavaType getType(final JavaMember javaMember) {
+		if (javaMember instanceof JavaField) {
+			return ((JavaField) javaMember).getType();
+		} else if (javaMember instanceof JavaMethod) {
+			return ((JavaMethod) javaMember).getReturnType();
+		} else {
+			throw new IllegalArgumentException(String.format("A JavaMember (%s) annotated with '%s' is neither a field nor a method.", javaMember, AggregateMember.class.getSimpleName()));
+		}
+	}
+
+	private JavaClass getJavaClass(final JavaType type) {
+		final JavaClass erasure = type.toErasure();
+
+		if (type instanceof JavaParameterizedType && erasure.isAssignableTo(Collection.class)) {
+			final JavaType collectionType = ((JavaParameterizedType) type).getActualTypeArguments().get(0);
+			return collectionType.toErasure();
+		}
+		if (type instanceof JavaParameterizedType && erasure.isAssignableTo(Map.class)) {
+			final JavaType valueType = ((JavaParameterizedType) type).getActualTypeArguments().get(1);
+			return valueType.toErasure();
+		}
+
+		return erasure;
+	}
+	
+	private boolean isNotUntypedCollectionOrMap(final JavaType type) {
+		final boolean isUntypedCollection = type.toErasure().isAssignableTo(Collection.class) && !(type instanceof JavaParameterizedType);
+		final boolean isUntypedMap = type.toErasure().isAssignableTo(Map.class) && !(type instanceof JavaParameterizedType);
+		return !(isUntypedCollection || isUntypedMap);
+	}
+
+}
---
+++ integration/axon/src/main/java/org/archifacts/integration/axon/SagaDescriptor.java
@@ -0,0 +1,25 @@
+package org.archifacts.integration.axon;
+
+import org.archifacts.core.descriptor.BuildingBlockDescriptor;
+import org.archifacts.core.model.BuildingBlockType;
+import org.axonframework.modelling.saga.StartSaga;
+
+import com.tngtech.archunit.core.domain.JavaClass;
+
+public class SagaDescriptor implements BuildingBlockDescriptor {
+
+	private static final BuildingBlockType TYPE = BuildingBlockType.of("Saga");
+
+	@Override
+	public BuildingBlockType type() {
+		return TYPE;
+	}
+
+	@Override
+	public boolean isBuildingBlock(JavaClass javaClass) {
+		return javaClass.getMethods()
+				.stream()
+				.anyMatch(method -> method.isMetaAnnotatedWith(StartSaga.class));
+	}
+
+}
