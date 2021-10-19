9ef7d683794098c9b43412675cd9bcdcb19da979
Oliver Libutzki <oliver@libutzki.de>
2021-10-19 20:06:07 +0200

    ComponentDescriptor Refactoring

--- integration/spring/src/main/java/org/archifacts/integration/spring/SpringDescriptors.java
+++ integration/spring/src/main/java/org/archifacts/integration/spring/SpringDescriptors.java
@@ -3,6 +3,7 @@ package org.archifacts.integration.spring;
 import org.archifacts.core.descriptor.BuildingBlockDescriptor;
 import org.archifacts.core.model.BuildingBlockType;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public final class SpringDescriptors {
 		public static final BuildingBlockDescriptor ServiceDescriptor = BuildingBlockDescriptor.forMetaAnnotatedWith(BuildingBlockType.of("Service"), Service.class);
 		public static final BuildingBlockDescriptor ConfigurationDescriptor = BuildingBlockDescriptor.forMetaAnnotatedWith(BuildingBlockType.of("Configuration"), Configuration.class);
 		public static final BuildingBlockDescriptor ControllerDescriptor = BuildingBlockDescriptor.forMetaAnnotatedWith(BuildingBlockType.of("Controller"), Controller.class);
-		public static final BuildingBlockDescriptor ComponentDescriptor = new ComponentDescriptor();
+		public static final BuildingBlockDescriptor ComponentDescriptor = BuildingBlockDescriptor.forMetaAnnotatedWith(BuildingBlockType.of("Component"), Component.class);
 
 	}
 
