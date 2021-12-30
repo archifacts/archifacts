ce645e35b6c81c51a04443c1bfc031436866261d
Oliver Libutzki <oliver@libutzki.de>
2021-12-31 00:16:13 +0100

    Enhancements

--- core/src/main/java/org/archifacts/core/model/HasContainers.java
+++ core/src/main/java/org/archifacts/core/model/HasContainers.java
@@ -2,6 +2,8 @@ package org.archifacts.core.model;
 
 import static org.archifacts.core.model.ArchifactsCollectors.toUnmodifiableLinkedSet;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 public interface HasContainers {
@@ -9,9 +11,14 @@ public interface HasContainers {
 	Set<ArtifactContainer> getContainers();
 
 	default Set<ArtifactContainer> getContainersOfType(final ArtifactContainerType artifactContainerType) {
+		return getContainersOfTypes(artifactContainerType);
+	}
+	
+	default Set<ArtifactContainer> getContainersOfTypes(final ArtifactContainerType... artifactContainerTypes) {
+		final List<ArtifactContainerType> artifactContainerTypeList = Arrays.asList(artifactContainerTypes);
 		return getContainers()
 				.stream()
-				.filter(container -> container.getType().equals(artifactContainerType))
+				.filter(container -> artifactContainerTypeList.contains(container.getType()))
 				.collect(toUnmodifiableLinkedSet());
 	}
 
